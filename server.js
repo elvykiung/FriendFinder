bf400485d2f1a651458ff9caec1fb5a03ba80386
Elvy Kiung <kiungh22@gmail.com>
2019-06-22 22:14:37 -0700

    fix file name and route problem

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 // LISTENER
 // The below code effectively "starts" our server
